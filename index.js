7ac9a0ff80d6ee2ca885f382aacc1236cf7be98a
BeastTheNinja <BeastTheNinja21@outlook.com>
2025-11-19 13:28:19 +0100

    Test Commit med merge

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import http from "http";
 http
   .createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Goddag med jer i verden!");
+    response.end("ny dag i denne verden!");
     console.log('Server responded with "Hello, world!"');
   })
   .listen(4000, () => {
