1e4ac9aa286963aa84b68495f33f2dc20d6afdf2
BeastTheNinja <BeastTheNinja21@outlook.com>
2025-11-20 14:43:40 +0100

    added an env file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 import { carRouter } from "./routes/carRoute.js";
 import { aboutRouter } from "./routes/aboutRoute.js";
 import { departmentRouter } from "./routes/departmentRoute.js";
 import { contactRouter } from "./routes/contactRoute.js";
 
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.SERVERPORT || 3000;
 
 const app = express();
 
@@ -14,10 +16,10 @@ app.get("/", (req, res) => {
   console.log("yoyo");
 });
 
-app.use('/cars', carRouter);
-app.use('/about', aboutRouter);
-app.use('/departments', departmentRouter);
-app.use('/contact', contactRouter);
+app.use("/cars", carRouter);
+app.use("/about", aboutRouter);
+app.use("/departments", departmentRouter);
+app.use("/contact", contactRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
