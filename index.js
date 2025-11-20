83498d6cc4dff2a7a6fb2bc30104f953873fe8bc
BeastTheNinja <BeastTheNinja21@outlook.com>
2025-11-20 10:56:48 +0100

    added routing

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-import http from "http";
-const port = 4000;
-http
-  .createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("verden!");
-    console.log('Server responded with "Hello, world!"');
-  })
-  .listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+import express from "express";
+
+const ROOT = 3000;
+const ABOUT = 4000;
+const CONTACT = 5000;
+const CARS = 6000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("dav dav");
+  console.log("yoyo");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about page");
+  console.log("about pages here ");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("contact page");
+  console.log("contact pages here ");
+});
+
+app.get("/cars", (req, res) => {
+  res.send("cars page");
+  console.log("cars pages here ");
+});
+
+app.listen(ROOT, () => {
+  console.log(`Server is running on http://localhost:${ROOT}`);
+});
