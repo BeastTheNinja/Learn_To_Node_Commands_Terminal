771028027fd4d54d4959875527dbbf1a8d798132
BeastTheNinja <BeastTheNinja21@outlook.com>
2025-11-24 13:56:02 +0100

    updated carController with a create

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,11 +1,26 @@
 import { prisma } from "../prismaClient.js";
 
 export const createRecord = async (req, res) => {
-    console.log(1212);
-    
   const { category, brand, model, year, price, fueltype } = req.body;
 
   if (!category || !brand || !model || !year || !price || !fueltype) {
-    return res.status(400).send("All fields are required");
+    return res.status(400).json("All fields are required");
+  }
+
+  try {
+    const data = await prisma.cars.create({
+      data: {
+        category,
+        brand,
+        model,
+        year: Number(year),
+        price,
+        fueltype,
+      },
+    });
+    res.status(201).json(data);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "NOGET GIK GALT VED OPRETTELSE AF BIL" });
   }
 };
